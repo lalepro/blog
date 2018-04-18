baac5e13215c44003e160c46dc2b52bfd8c876a2
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-18 11:00:12 -0500

    created a service dependency injection

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.example.blog.controllers;
 
 import com.example.blog.models.Post;
+import com.example.blog.services.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,27 +11,27 @@ import java.util.List;
 
 @Controller
 public class PostController {
-//    private final PostService postSvc;
-//
-//    public PostsController(PostService postSvc) {
-//        this.postSvc = postSvc;
-//    }
+    private final PostService postSvc;
+
+    public PostController(PostService postSvc) {
+        this.postSvc = postSvc;
+    }
 
     @GetMapping("/posts")
     public String index( Model model){
-                List<Post> posts = new ArrayList<>();
-        Post post1 = new Post("first title", "first body");
-        Post post2 = new Post("second title", "second body");
-        posts.add(post1);
-        posts.add(post2);
-        model.addAttribute("posts", posts);
+//                List<Post> posts = new ArrayList<>();
+//        Post post1 = new Post(1, "first title", "first body");
+//        Post post2 = new Post(2, "second title", "second body");
+//        posts.add(post1);
+//        posts.add(post2);
+        model.addAttribute("posts", postSvc.findAll());
         return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String show(@PathVariable Integer id, Model model){
-//        Post post = new Post(1L, "first post title", "first post body");
-        model.addAttribute("id", id);
+//        Post post = new Post(1,"first post title", "first post body");
+        model.addAttribute("post", postSvc.findOne(id));
         return "/posts/show";
     }
 
@@ -43,7 +44,7 @@ public class PostController {
 
     @PostMapping("/posts/create")
     public String insert( @ModelAttribute Post post){
-//        postSvc.save(post);
+        postSvc.save(post);
         return "redirect:/posts";
     }
 }
