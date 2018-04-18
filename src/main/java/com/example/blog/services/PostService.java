205d24edccd43fca06889e1656ebaa54a2f229cd
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-18 11:02:12 -0500

    created a Service class for the posts

--- src/main/java/com/example/blog/services/PostService.java
+++ src/main/java/com/example/blog/services/PostService.java
@@ -1,4 +1,37 @@
 package com.example.blog.services;
 
+import com.example.blog.models.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PostService {
+    private List<Post> posts;
+
+    public PostService(){
+        posts = new ArrayList<>();
+        createAds();
+    }
+    public List<Post> findAll(){
+        return posts;
+    }
+
+    public Post save(Post post){
+        post.setId(posts.size() + 1);
+        posts.add(post);
+        return post;
+    }
+
+    public Post findOne(int id){
+        return posts.get(id -1);
+    }
+
+    private void createAds(){
+        save(new Post("Great Day to Code", "Why Hike or Bike when you can trek in tech land"));
+        save(new Post("Hump Day!", "When you want it to be Friday but it's not"));
+        save(new Post("4 weeks to Capstone!", "I'm Fine! Fake it til ya make it!"));
+
+    }
 }
