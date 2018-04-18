603a8cbdbd57f77358d1d42166e8a501017f3ed9
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-17 22:57:09 -0500

    save postController changes.took off response body so controller can link to a html view file

--- src/main/java/com/example/blog/controllers/PostController.java
+++ src/main/java/com/example/blog/controllers/PostController.java
@@ -1,31 +1,49 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
+    private final PostService postSvc;
+
+    public PostsController(PostService postSvc) {
+        this.postSvc = postSvc;
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "posts index page";
+    public String index( Model model){
+                List<Post> posts = new ArrayList<>();
+        Post post1 = new Post(1L,"first title", "first body");
+        Post post2 = new Post(2L, "second title", "second body");
+        posts.add(post1);
+        posts.add(post2);
+//        model.addAttribute("posts", postSvc.findAll());
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String show(@PathVariable Integer id){
-        return "Return id " + id;
+    public String show(@PathVariable Integer id, Model model){
+//        Post post = new Post(1L, "first post title", "first post body");
+        model.addAttribute("id", id);
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String create(){
-        return "view the form for creating a post";
+    public String create(Model model){
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "/posts/create";
     }
 
     @PostMapping("/posts/create")
-    @ResponseBody
-    public String postCreate(@RequestParam String name){
-        return "create a new post: " + name;
+    public String insert( @ModelAttribute Post post){
+        postSvc.save(post);
+        return "redirect:/posts";
     }
 }
