3a703579e098d7e160fff166710929d2128364e4
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-19 11:55:46 -0500

    morning exercise

---
+++ seeder.sql
@@ -0,0 +1,33 @@
+
+
+
+
+
+USE dog_user;
+
+# TRUNCATE dogs;
+# TRUNCATE people;
+
+INSERT INTO dogs (age, name, reside_state)
+VALUES
+  (2, 'Chuck', 'TX'),
+  (5, 'Fred', 'OH'),
+  (2, 'Bud', 'TN'),
+  (2, 'Bailey', 'AL'),
+  (4, 'Lexie', 'TX'),
+  (10, 'Snickers', 'TX'),
+  (6, 'Red', 'FL'),
+  (8, 'Barney', 'CA'),
+  (1, 'Bowser', 'TX');
+
+INSERT INTO people (age, name, reside_state)
+VALUES
+  (22, 'John', 'TX'),
+  (35, 'Kyle', 'OH'),
+  (12, 'Kevin', 'TN'),
+  (52, 'Daniel', 'AL'),
+  (74, 'Sharon', 'TX'),
+  (10, 'Michael', 'TX'),
+  (46, 'Sam', 'FL'),
+  (48, 'Cathy', 'CA'),
+  (31, 'Bowser', 'TX');
