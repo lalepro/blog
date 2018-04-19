59aac32b641720818c3259379331e327869a306a
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-19 11:56:48 -0500

    working on crud repo

--- src/main/java/com/example/blog/daos/PostsRepository.java
+++ src/main/java/com/example/blog/daos/PostsRepository.java
@@ -1,7 +1,16 @@
 package com.example.blog.daos;
 
 import com.example.blog.models.Post;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+//TODO: JPA #4 Create an interface for posts that extends CrudRepository
+@Repository
 public interface PostsRepository extends CrudRepository<Post, Long> {
+//    @Override
+//    Iterable<Post> findAll(Iterable<Long> iterable);
+
+//    Iterable<Post> findAllById(Iterable<Long> iterable);
+
 }
