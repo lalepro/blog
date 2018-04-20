4e1bc6e0e62bb57aceebfb556832f5fb2a8d1386
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-20 09:54:52 -0500

    added a model class for Users

--- src/main/java/com/example/blog/models/User.java
+++ src/main/java/com/example/blog/models/User.java
@@ -1,4 +1,77 @@
 package com.example.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+//    TODO: @Email annotation
+    private String email;
+
+    @Column(nullable = false)
+//    TODO: annotation for length, and characters needed, uppercase, lowercase
+    private String password;
+
+//    TODO: Separate concerns in relationships postDetails
+//    @OneToOne
+//    private
+
+//TODO: 3 constructors and getters and setters
+
+
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
