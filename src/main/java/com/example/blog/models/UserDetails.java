5b556f293226b5c5f155022b7a30153f5fdf3b94
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-20 12:30:54 -0500

    created model for more user info

--- src/main/java/com/example/blog/models/UserDetails.java
+++ src/main/java/com/example/blog/models/UserDetails.java
@@ -1,4 +1,27 @@
 package com.example.blog.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_details")
 public class UserDetails {
+    @Id @GeneratedValue
+    private long id;
+
+    @Column(columnDefinition = "Text")
+    private String address;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(columnDefinition = "TEXT")
+    private String path;
+
+    @OneToOne
+    private User user;
+
+
+
+
+
 }
