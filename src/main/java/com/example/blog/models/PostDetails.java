cec378b502527d533063f41b765f38c071795e2a
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-20 20:39:41 -0500

    post details offer a comments section for post and location of comment

--- src/main/java/com/example/blog/models/PostDetails.java
+++ src/main/java/com/example/blog/models/PostDetails.java
@@ -3,7 +3,7 @@ package com.example.blog.models;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "post")
+@Table(name = "post_details")
 public class PostDetails {
 
     @Id @GeneratedValue
@@ -12,6 +12,56 @@ public class PostDetails {
     @Column
     private String location;
 
+    @Column(columnDefinition = "Text")
+    private String question;
 
+    @Column(columnDefinition = "Text")
+    private String answer;
 
+    public PostDetails() {
+    }
+
+    public PostDetails(String location, String question, String answer) {
+        this.location = location;
+        this.question = question;
+        this.answer = answer;
+    }
+    public PostDetails(long id, String location, String question, String answer) {
+        this.id = id;
+        this.location = location;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
