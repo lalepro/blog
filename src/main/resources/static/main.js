635878624e3ef868386a13da213a5d1304447ed0
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-04-21 14:49:17 -0500

    added vanilla js for homepage animation

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,11 @@
+var mug = document.querySelector("img");
+var angle = 0, lastTime = null;
+function animate(time){
+    if (lastTime != null)
+        angle += (time - lastTime) * 0.0007;
+    lastTime = time;
+    mug.style.top = (Math.sin(angle) * 300) + "px";
+    mug.style.right = (Math.cos(angle) * 300) + "px";
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
